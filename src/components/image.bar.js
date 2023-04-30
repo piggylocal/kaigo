c4ac9ba57ba4a5f21ee0462f5cccc7363c110c6b
acst1223 <natnstart@gmail.com>
2023-05-01 01:29:28 +0900

    Feat: ImageBar

--- src/components/image.bar.js
+++ src/components/image.bar.js
@@ -3,8 +3,8 @@ import ImageLinkComponent from "./image.link";
 const ImageBarComponent = () => {
     return (
         <div id="image-bar">
-            <ImageLinkComponent src="https://i.imgur.com/eww4OwG.jpg" description="合唱団"/>
-            <ImageLinkComponent src="https://i.imgur.com/yd6sGar.jpg" description="お風呂"/>
+            <ImageLinkComponent src="https://i.imgur.com/gehrpNR.jpg" description="合唱団" />
+            <ImageLinkComponent src="https://i.imgur.com/yd6sGar.jpg" description="お風呂" />
         </div>
     );
 };
