c3039fe6d26352a093871117034783a462e351fc
acst1223 <natnstart@gmail.com>
2023-04-30 16:24:52 +0900

    Feat: Font for main text

--- src/components/title.js
+++ src/components/title.js
@@ -1,9 +1,9 @@
 const TitleComponent = () => {
     return (
         <div id="title">
-            <p style={{marginTop: "0"}}>この国を</p>
-            <p style={{marginTop: "3vw"}}>守った人を</p>
-            <p style={{marginTop: "6vw"}}>守りたい。</p>
+            <p style={{marginTop: "0", paddingBottom: "0.8vw"}}>この国を</p>
+            <p style={{marginTop: "3vw", paddingTop: "1vw", paddingBottom: "0.8vw"}}>守った人を</p>
+            <p style={{marginTop: "6vw", paddingTop: "1vw", paddingBottom: "0vw", maxHeight: "16vw"}}>守りたい。</p>
         </div>
     )
 };
