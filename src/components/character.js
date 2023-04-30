0e1360f0ced6154113aa86869cf0a440cfe0cc05
acst1223 <natnstart@gmail.com>
2023-05-01 00:02:43 +0900

    Feat: Characters

---
+++ src/components/character.js
@@ -0,0 +1,9 @@
+const CharacterComponent = () => {
+    return (<div id="character">
+        <img src="https://i.imgur.com/K1E9Sed.png" alt="" className="character character-left" />
+        <img src="https://i.imgur.com/rSrU4si.png" alt="" className="character character-middle" />
+        <img src="https://i.imgur.com/NOl22a3.png" alt="" className="character character-right" />
+    </div>);
+};
+
+export default CharacterComponent;
--- src/components/home.js
+++ src/components/home.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import CoverComponent from "./cover";
 import MainTextComponent from "./main.text";
+import CharacterComponent from "./character";
 
 const HomeComponent = () => {
     return (
         <div>
             <CoverComponent />
+            <CharacterComponent />
             <MainTextComponent />
         </div>
     )
