c1de129d2749be4843f917d7144bf2573ec5ee58
acst1223 <natnstart@gmail.com>
2023-05-11 00:02:49 +0900

    Fix: image.bar

--- src/components/image.bar.js
+++ src/components/image.bar.js
@@ -3,8 +3,8 @@ import ImageLinkComponent from "./image.link";
 const ImageBarComponent = () => {
     return (
         <div id="image-bar">
-            <ImageLinkComponent src="https://i.imgur.com/gehrpNR.jpg" description="合唱団" />
-            <ImageLinkComponent src="https://i.imgur.com/5KcZQvl.jpg" description="お風呂" />
+            <ImageLinkComponent src="https://i.imgur.com/UwlOb17.jpg" description="合唱団" />
+            <ImageLinkComponent src="https://i.imgur.com/QF8kiaI.jpg" description="お風呂" />
         </div>
     );
 };
