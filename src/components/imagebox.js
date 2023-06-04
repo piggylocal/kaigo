e8f7f72ece3fa7d7cef37e284969f947b0af843d
acst1223 <natnstart@gmail.com>
2023-06-05 00:50:08 +0900

    Feat: New image in image.bar

--- src/components/imagebox.js
+++ src/components/imagebox.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect} from "react";
 
 const ImgboxComponent = () => {
     const imgUrls = [
-        "https://i.imgur.com/QF8kiaI.jpg",
-        "https://i.imgur.com/UwlOb17.jpg",
-        "https://i.imgur.com/osAsQ00.jpg"
+        "https://i.imgur.com/LUola0Z.jpg", // 吃
+        "https://i.imgur.com/xK2q1ja.jpg", // 盖被子
+        "https://i.imgur.com/DnJs5k0.jpg", // 洗bb
+        "https://i.imgur.com/oPBXmQf.jpg",
+        "https://i.imgur.com/A76DhKc.jpg"
     ];
     const opacities = Array(imgUrls.length).fill(0);
     const [imageIndex, setImageIndex] = useState(0);
