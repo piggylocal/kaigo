41c6f0b2d6457f5f876b7cba65e7a206697c7e48
acst1223 <natnstart@gmail.com>
2023-06-05 13:59:11 +0900

    Feat: Image bar loop

--- src/components/image.bar.js
+++ src/components/image.bar.js
@@ -1,10 +1,21 @@
 import ImageLinkComponent from "./image.link";
 
 const ImageBarComponent = () => {
+    const imgUrls = [
+        {url: "https://i.imgur.com/yfWUuS2.jpg", desc: "食事介助"},
+        {url: "https://i.imgur.com/M6Awrq7.jpg", desc: "入浴介助"},
+        {url: "https://i.imgur.com/kfW0vhB.jpg", desc: "レクリエーション"},
+        {url: "https://i.imgur.com/xMFAqbt.jpg", desc: "今日も綺麗"},
+        {url: "https://i.imgur.com/s1K9SQP.jpg", desc: "就寝の見回り"},
+        {url: "https://i.imgur.com/FRg9b1q.jpg", desc: "楽しい日々"},
+    ];
+
     return (
         <div id="image-bar">
-            <ImageLinkComponent src="https://i.imgur.com/UwlOb17.jpg" description="合唱団" />
-            <ImageLinkComponent src="https://i.imgur.com/QF8kiaI.jpg" description="お風呂" />
+            <ImageLinkComponent />
+            {imgUrls.map((imgUrl, index) => {return (
+                <ImageLinkComponent src={imgUrl.url} description={imgUrl.desc} index={index} total={imgUrls.length} />
+            )})}
         </div>
     );
 };
--- src/components/image.link.js
+++ src/components/image.link.js
@@ -1,8 +1,9 @@
-const ImageLinkComponent = ({ src, description }) => {
+const ImageLinkComponent = ({ src, description, total, index }) => {
+    let animationDuration = 48;
     return (
-        <div className="image-link">
+        <div className="image-link" style={{animation: `image-move ${animationDuration}s linear -${animationDuration / total * index}s infinite`}}>
             <div className="image-link-container">
-                <div className="image-link-image" style={{backgroundImage: `url("${src}")`}}></div>
+                <div className="image-link-image" style={{backgroundImage: src ? `url("${src}")` : ""}}></div>
             </div>
             <div className="image-link-description">{description}</div>
         </div>
