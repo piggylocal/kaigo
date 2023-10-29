78e83838413915f0612e4b413abf0848747ec096
acst1223 <natnstart@gmail.com>
2023-10-29 22:50:36 +0900

    Fix: press the hyperlink and scroll to the top

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import MenuComponent from "./components/menu";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
 
 function App() {
+
   return (
     <div className="App">
+        <ScrollToTop />
         <Outlet />
         <MenuComponent />
     </div>
